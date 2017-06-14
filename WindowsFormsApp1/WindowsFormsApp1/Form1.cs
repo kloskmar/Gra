cd3ebdc2c1b0cdc2917f5b4ddb4745fdca03523e
Marcin <kloskmar@gmail.com>
2017-06-14 20:15:10 +0200

    panele w tablicy działają mechanizm losujacy jeszcze nie

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -15,11 +15,57 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         Random random = new Random();
-        int[] arr = Enumerable.Range(0, 35).ToArray();
+        int[] arr = Enumerable.Range(0, 36).ToArray();
+        Panel[] panel = new Panel[37];
+        
+
+
+        
 
         public Form1()
         {
             InitializeComponent();
+            panel[1] = a1;
+            panel[2] = a2;
+            panel[3] = a3;
+            panel[4] = a4;
+            panel[5] = a5;
+            panel[6] = a6;
+            panel[7] = b1;
+            panel[8] = b2;
+            panel[9] = b3;
+            panel[10] = b4;
+            panel[11] = b5;
+            panel[12] = b6;
+            panel[13] = c1;
+            panel[14] = c2;
+            panel[15] = c3;
+            panel[16] = c4;
+            panel[17] = c5;
+            panel[18] = c6;
+            panel[19] = d1;
+            panel[20] = d2;
+            panel[21] = d3;
+            panel[22] = d4;
+            panel[23] = d5;
+            panel[24] = d6;
+            panel[25] = e1;
+            panel[26] = e2;
+            panel[27] = e3;
+            panel[28] = e4;
+            panel[29] = e5;
+            panel[30] = e6;
+            panel[31] = f1;
+            panel[32] = f2;
+            panel[33] = f3;
+            panel[34] = f4;
+            panel[35] = f5;
+            panel[36] = f6;
+
+            for (int i=0; i <= 34; i++)
+            {
+                arr[i] = arr[i] % 4;
+            }
             
         }
 
@@ -92,6 +138,20 @@ namespace WindowsFormsApp1
         private void Start_Click(object sender, EventArgs e)
         {
             arr = arr.OrderBy(c => random.Next()).ToArray();
+            for (int i = 0; i <= 34; i++)
+            {
+                if (arr[i] == 1)
+                {
+                    panel[i+1].BackColor = Color.Orange;
+                }
+            }
+            if (arr[1]==2)
+            {
+                panel[1].BackColor = Color.Blue;
+            }
+            panel[2].BackColor = Color.Blue;
+
+
         }
     }
 
